cfd374cfc2fe348a68c559bf3527a4c44d619260
Clif Reeder <clif@voxmedia.com>
2014-12-28 15:56:29 -0500

    new deploy:stop scheme

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require 'shellwords'
+
 set :application, "albums"
 set :repository,  "git://github.com/clifff/albums_of_the_year.git"
 set :scm, :git
@@ -31,7 +33,19 @@ namespace :deploy do
     sudo "sh -c 'cd #{latest_release} && fig -f #{fig_file} up -d'"
   end
   task :stop, :roles => :web, :except => { :no_release => true } do
-    sudo "sh -c 'cd #{latest_release} && fig -f #{fig_file} stop'"
+    # Force a sudo command so that capture doesn't later try to
+    # ask for the password
+    sudo("pwd")
+    # PROBLEM: When we specify a fig file, things like stop/ps/log ONLY look
+    # at the docker instances particular to that exact path, so instances created
+    # by previous releases don'ts how up. As such, we can't JUST 'fig stop' and
+    # try this instead
+    containers =  capture("sudo -p 'sudo password: ' sh -c 'docker ps -q'").split("\r")
+    containers.each do |container|
+      container = container.strip
+      next if container.nil? || container == ""
+      sudo("sh -c 'docker stop #{container}'")
+    end
   end
   task :restart, :roles => :web, :except => { :no_release => true } do
     stop
